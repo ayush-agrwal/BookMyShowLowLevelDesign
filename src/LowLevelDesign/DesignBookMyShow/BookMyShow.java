1cd8d49ebbb558e835a2ab204573ed50df042827
Ayush Agrawal <145705235+ayush-agrwal@users.noreply.github.com>
2024-08-14 23:27:12 +0530

    Update BookMyShow.java

--- src/LowLevelDesign/DesignBookMyShow/BookMyShow.java
+++ src/LowLevelDesign/DesignBookMyShow/BookMyShow.java
@@ -1,58 +1,59 @@
-Skip to content
-GitLab
-Menu
-Sign in
-shrayansh jain
-InterviewCodingPractise
-Repository
-interviewcodingpractise
-src
-LowLevelDesign
-DesignBookMyShow
-BookMyShow.java
-Jain, Shrayansh's avatar
-lld bookmyshow
-ArunodayamOhack authored 2 years ago
-fb729f3b
-BookMyShow.java
-6.12 KiB
 package LowLevelDesign.DesignBookMyShow;
+
 import LowLevelDesign.DesignBookMyShow.Enums.City;
 import LowLevelDesign.DesignBookMyShow.Enums.SeatCategory;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+
 public class BookMyShow {
+
     MovieController movieController;
     TheatreController theatreController;
+
     BookMyShow() {
         movieController = new MovieController();
         theatreController = new TheatreController();
     }
+
+
     public static void main(String args[]) {
+
         BookMyShow bookMyShow = new BookMyShow();
+
         bookMyShow.initialize();
+
         //user1
         bookMyShow.createBooking(City.Bangalore, "BAAHUBALI");
         //user2
         bookMyShow.createBooking(City.Bangalore, "BAAHUBALI");
+
     }
+
     private void createBooking(City userCity, String movieName) {
+
+
         //1. search movie by my location
         List<Movie> movies = movieController.getMoviesByCity(userCity);
+
         //2. select the movie which you want to see. i want to see Baahubali
         Movie interestedMovie = null;
         for (Movie movie : movies) {
+
             if ((movie.getMovieName()).equals(movieName)) {
                 interestedMovie = movie;
             }
         }
+
         //3. get all show of this movie in Bangalore location
         Map<Theatre, List<Show>> showsTheatreWise = theatreController.getAllShow(interestedMovie, userCity);
+
         //4. select the particular show user is interested in
         Map.Entry<Theatre,List<Show>> entry = showsTheatreWise.entrySet().iterator().next();
         List<Show> runningShows = entry.getValue();
         Show interestedShow = runningShows.get(0);
+
         //5. select the seat
         int seatNumber = 30;
         List<Integer> bookedSeats = interestedShow.getBookedSeatIds();
@@ -196,3 +197,4 @@ public class BookMyShow {
         movieController.addMovie(baahubali, City.Delhi);
     }
 }
+
